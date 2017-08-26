62a5934aba61dfc7207109342439ef7bec76f0db
Olive Mugeri <olive.nyotu@andela.com>
2017-08-26 22:05:37 +0300

    setup db connection

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = require('express')();
 const router = require('./server/routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongo.connect('mongodb://riwhiz:library@ds159953.mlab.com:59953/library');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
